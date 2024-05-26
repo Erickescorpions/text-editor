382277fded447dcab03ad86e35ffc299c999d209
Erick Vazquez <erickescorpions@comunidad.unam.mx>
2024-05-26 02:05:15 -0600

    codigo de ejemplo para probar la pila

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "stack/stack.h"
+
+int main() {
+  Action_t* action = create_action(5, INSERT, "Hola mundo");
+
+  Stack_t* s = create_stack(5);
+  push_stack(s, action);
+  Action_t* popped_action = pop_stack(s);
+
+
+  print_action(popped_action);
+}
